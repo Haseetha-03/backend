724410f53390a42e555b944e852023f3c167fbbb
Haseetha-03 <ahaladhinitummala@gmail.com>
2025-12-31 09:57:53 +0530

    finish

--- index.js
+++ index.js
@@ -43,3 +43,24 @@
 // app.listen(7007,()=>{
 //     console.log("Server  running at port 7000");
 // })
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import studentRouter from './routes/studentsRoutes.js';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://ahaladhinitummala_db_user:Haseetha20@cluster0.ohnvujh.mongodb.net/").then(() => console.log("db connected"))
+.catch((error) => console.log(error));
+
+app.use('/', studentRouter);
+app.get('/users', (req, res) => {
+    console.log("hello this is");
+    res.send("hello this is from backend");
+})
+
+app.listen(2885, () => {
+    console.log("server running at port 2885")
+});
